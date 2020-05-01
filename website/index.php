a1e84e2c335956064fba139a42db4d84cfa51752
EdenilsonAndrade <edenilsonbass@hotmail.com>
2020-05-01 14:38:59 -0300

    Até a aula 88

--- website/index.php
+++ website/index.php
@@ -1,5 +1,23 @@
 <?php 
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+// aqui seria quando for acessar a página principal
+$app->get('/', function(){
+
+	echo json_encode(array(
+		'date'=>date("Y-m-d H:i:s")
+	));
+});
+
+// aqui irá retornar se for utilizado no navegador o endereço http://localhost/website/hello/Hcode
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
 
  ?>
